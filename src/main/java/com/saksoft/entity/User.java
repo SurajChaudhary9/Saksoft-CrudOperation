3e229809b2a7d63596820bf7c1a8cc87dce62c4e
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2023-04-19 18:33:26 +0530

    [Suraj] Add Entity

---
+++ src/main/java/com/saksoft/entity/User.java
@@ -0,0 +1,22 @@
+package com.saksoft.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name ="User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+}
